c20b5aa171ad1869b1aff1f71b077e630104b2e9
tristan.zhou <jiahuan.zhou@yahoo.com>
2019-04-04 09:45:49 +1300

    add prod config

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -19,7 +19,7 @@ class Fib extends Component {
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('api/values/all');
+    const seenIndexes = await axios.get('/api/values/all');
     this.setState({
       seenIndexes: seenIndexes.data
     });
